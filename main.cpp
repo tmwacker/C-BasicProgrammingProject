8a6d93b4cf74a6ad151693e44fc60119a6494f97
tmwacker <tmw24859@email.vccs.edu>
2024-09-21 00:51:31 +0000

    Finished

--- main.cpp
+++ main.cpp
@@ -1,3 +1,11 @@
 #include <iostream>
-
-int main() { std::cout << "Hello World!\n"; }
+using namespace std;
+int main() 
+{
+ int town;
+  town=20 * 23.5;
+ int highway;
+  highway=20* 28.9;
+  cout <<"The number of miles you can travel in town is " << town << endl; 
+  cout <<"The number of miles you can travel on the highway is " << highway << endl; 
+}
